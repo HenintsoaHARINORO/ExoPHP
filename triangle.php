0e66ea229d7e1e1393eb80800cff2524245c5657
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-03-11 15:56:22 +0300

    path

--- triangle.php
+++ triangle.php
@@ -3,7 +3,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <link rel="stylesheet" href="style1.css">
+    <link rel="stylesheet" href="css/tyle1.css">
     <link rel="stylesheet" href="bootstrap-4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="bootstrap-4.3.1/css/bootstrap.css">
 </head>
