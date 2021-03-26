bb42363d63f4c33006362f3a1e229950a2ec92f4
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-03-26 19:12:08 +0300

    removed cache

--- test.php
+++ test.php
@@ -6,9 +6,6 @@ use Twig\Loader\FilesystemLoader;
 
 // Initialize template engine https://twig.symfony.com/
 $loader = new FilesystemLoader('templates');
-$twig = new Environment($loader, [
-    'cache' => 'cache/twig',
-]);
 
 if(isset($_POST['vala'])) {
      echo $nums=$_POST['vala'];
